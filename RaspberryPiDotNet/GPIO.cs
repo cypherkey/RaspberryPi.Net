6067c19e5492a15c0c41ed02265db7adac8289b6
Aaron Anderson <github@netopia.ca>
2012-06-30 00:17:46 -0600

    Adding initial files

---
+++ RaspberryPiDotNet/GPIO.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// Author: Aaron Anderson <aanderson@netopia.ca>
+namespace RaspberryPiDotNet
+{
+    public abstract class GPIO : IDisposable
+    {
+        /// <summary>
+        /// Abstract class for the GPIO connector on the Pi (P1) (as found next to the yellow RCA video socket on the Rpi circuit board)
+        /// </summary>
+        /// <remarks>
+        /// Refer to http://elinux.org/Rpi_Low-level_peripherals for diagram.
+        /// P1-01 = bottom left, P1-02 = top left
+        /// pi connector P1 pin    = GPIOnum
+        ///                  P1-03 = GPIO0
+        ///                  P1-05 = GPIO1
+        ///                  P1-07 = GPIO4
+        ///                  P1-08 = GPIO14
+        ///                  P1-10 = GPIO15
+        ///                  P1-11 = GPIO17
+        ///                  P1-12 = GPIO18
+        ///                  P1-13 = GPIO21
+        ///                  P1-15 = GPIO22
+        ///                  P1-16 = GPIO23
+        ///                  P1-18 = GPIO24
+        ///                  P1-19 = GPIO10
+        ///                  P1-21 = GPIO9
+        ///                  P1-22 = GPIO25
+        ///                  P1-23 = GPIO11
+        ///                  P1-24 = GPIO8
+        ///                  P1-26 = GPIO7
+        /// So to turn on Pin7 on the GPIO connector, pass in enumGPIOPIN.gpio4 as the pin parameter
+        /// </remarks>
+        public enum GPIOPins
+        {
+            GPIO_NONE = -1,
+            GPIO00 = 0,
+            GPIO01 = 1,
+            GPIO04 = 4,
+            GPIO07 = 7,
+            GPIO08 = 8,
+            GPIO09 = 9,
+            GPIO10 = 10,
+            GPIO11 = 11,
+            GPIO14 = 14,
+            GPIO15 = 15,
+            GPIO17 = 17,
+            GPIO18 = 18,
+            GPIO21 = 21,
+            GPIO22 = 22,
+            GPIO23 = 23,
+            GPIO24 = 24,
+            GPIO25 = 25,
+            Pin03 = 0,
+            Pin05 = 1,
+            Pin07 = 4,
+            Pin08 = 14,
+            Pin10 = 15,
+            Pin11 = 17,
+            Pin12 = 18,
+            Pin13 = 21,
+            Pin15 = 22,
+            Pin16 = 23,
+            Pin18 = 24,
+            Pin19 = 10,
+            Pin21 = 9,
+            Pin22 = 25,
+            Pin23 = 11,
+            Pin24 = 8,
+            Pin26 = 7
+        };
+
+        /// <summary>
+        /// Specifies the direction of the GPIO port
+        /// </summary>
+        public enum DirectionEnum { IN, OUT };
+
+        /// <summary>
+        /// The currently assigned GPIO pin. Used for class methods and not static methods.
+        /// </summary>
+        protected GPIOPins _pin;
+
+        /// <summary>
+        /// Creates a GPIOFile class with 
+        /// </summary>
+        /// <param name="pin"></param>
+        public GPIO(GPIOPins pin)
+            : this(pin,DirectionEnum.OUT,false)
+        {
+        }
+
+        public GPIO(GPIOPins pin, DirectionEnum direction)
+            : this(pin, direction, false)
+        {
+        }
+
+        public GPIO(GPIOPins pin, DirectionEnum direction, bool initialValue)
+        {
+            this._pin = pin;
+        }
+
+        protected static string GetGPIONumber(GPIOPins pin)
+        {
+            return ((int)pin).ToString(); //e.g. returns 17 for enum value of gpio17
+        }
+
+        /// <summary>
+        /// Write a value to the pin
+        /// </summary>
+        /// <param name="value">The value to write to the pin</param>
+        public abstract void Write(bool value);
+
+        /// <summary>
+        /// Read a value from the pin
+        /// </summary>
+        /// <returns>The value read from the pin</returns>
+        public abstract bool Read();
+
+        /// <summary>
+        /// Dispose of the GPIO pin
+        /// </summary>
+        public abstract void Dispose();
+    }
+}
---
+++ RaspberryPiDotNet/GPIOFile.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+
+// Author: Aaron Anderson <aanderson@netopia.ca>
+// Based on work done by x4m and britguy (http://www.raspberrypi.org/phpBB3/viewtopic.php?f=34&t=6720)
+// DL: https://dl.dropbox.com/u/7610280/GPIO.cs
+namespace RaspberryPiDotNet
+{
+    public class GPIOFile : GPIO, IDisposable
+    {
+        /// <summary>
+        /// The path on the Raspberry Pi for the GPIO interface
+        /// </summary>
+        private const string GPIO_PATH = "/sys/class/gpio/";
+
+        private static Dictionary<int, DirectionEnum> _exported = new Dictionary<int, DirectionEnum>();
+
+        /// <summary>
+        /// Access to the specified GPIO setup as an output port with an initial value of false (0)
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        public GPIOFile(GPIOPins pin)
+            : this(pin,DirectionEnum.OUT,false)
+        {
+        }
+
+        /// <summary>
+        /// Access to the specified GPIO setup with the specified direction with an initial value of false (0)
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        /// <param name="direction">Direction</param>
+        public GPIOFile(GPIOPins pin, DirectionEnum direction)
+            : this(pin, direction, false)
+        {
+        }
+
+        /// <summary>
+        /// Access to the specified GPIO setup with the specified direction with the specified initial value
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        /// <param name="direction">Direction</param>
+        /// <param name="initialValue">Initial Value</param>
+        public GPIOFile(GPIOPins pin, DirectionEnum direction, bool initialValue)
+            : base(pin, direction, initialValue)
+        {
+            ExportPin(pin, direction);
+            Write(pin, initialValue);
+        }
+
+        ~GPIOFile()
+        {
+            UnexportPin(_pin);
+        }
+
+        #region Static Methods
+        /// <summary>
+        /// Export the GPIO setting the direction. This creates the /sys/class/gpio/gpioXX directory.
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        /// <param name="direction"></param>
+        private static void ExportPin(GPIOPins pin, DirectionEnum direction)
+        {
+            // If the pin is already exported, check it's in the proper direction
+            if (_exported.Keys.Contains((int)pin))
+                // If the direction matches, return out of the function. If not, change the direction
+                if (_exported[(int)pin] == direction)
+                    return;
+                else
+                {
+                    File.WriteAllText(GPIO_PATH + "gpio" + GetGPIONumber(pin) + "/direction", direction.ToString().ToLower());
+                    _exported[(int)pin] = direction;
+                    return;
+                }
+
+            if (!Directory.Exists(GPIO_PATH + "gpio" + GetGPIONumber(pin)))
+            {
+                Debug.WriteLine("Exporting " + GetGPIONumber(pin));
+                //export
+                File.WriteAllText(GPIO_PATH + "export", GetGPIONumber(pin));
+            }
+
+            // set i/o direction
+            Debug.WriteLine("Setting direction on pin " + pin + "/gpio " + (int)pin + " as " + direction);
+            File.WriteAllText(GPIO_PATH + "gpio" + GetGPIONumber(pin) + "/direction", direction.ToString().ToLower());
+
+            // Update the list of exported pins
+            _exported[(int)pin] = direction;
+        }
+
+        /// <summary>
+        /// Unexport the GPIO. This removes the /sys/class/gpio/gpioXX directory.
+        /// </summary>
+        /// <param name="pin">The pin to unexport</param>
+        private static void UnexportPin(GPIOPins pin)
+        {
+            Debug.WriteLine("unexporting pin " + pin);
+            File.WriteAllText(GPIO_PATH + "unexport", GetGPIONumber(pin));
+
+            // Remove the pin from the list of exported pins
+            _exported.Remove((int)pin);
+        }
+
+        /// <summary>
+        /// Static method to write a value to the specified pin. When using the static methods ensure you use the CleanUp() method when done.
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        /// <param name="value">The value to write to the pin</param>
+        public static void Write(GPIOPins pin, bool value)
+        {
+            if (pin == GPIOPins.GPIO_NONE)
+                return;
+
+            ExportPin(pin, DirectionEnum.OUT);
+
+            string writeValue = value ? "1" : "0";
+            File.WriteAllText(GPIO_PATH + "gpio" + GetGPIONumber(pin) + "/value", writeValue);
+            Debug.WriteLine("output to pin " + pin + "/gpio " + (int)pin + ", value was " + value);
+        }
+
+        /// <summary>
+        /// Static method to read a value to the specified pin. When using the static methods ensure you use the CleanUp() method when done.
+        /// </summary>
+        /// <param name="pin">The GPIO pin</param>
+        /// <returns>The value read from the pin</returns>
+        public static bool Read(GPIOPins pin)
+        {
+            bool returnValue = false;
+
+            ExportPin(pin, DirectionEnum.IN);
+
+            string filename = GPIO_PATH + "gpio" + GetGPIONumber(pin) + "/value";
+            if (File.Exists(filename))
+            {
+                string readValue = File.ReadAllText(filename);
+                if (readValue.Length > 0 && readValue[0] == '1')
+                    returnValue = true;
+            }
+            else
+                throw new Exception(string.Format("Cannot read from {0}. File does not exist", pin));
+
+            Debug.WriteLine("input from pin " + pin + "/gpio " + (int)pin + ", value was " + returnValue);
+
+            return returnValue;
+        }
+
+        public static void CleanUp()
+        {
+            // Loop over all exported pins and unexported them
+            foreach (GPIOPins pin in _exported.Keys)
+            {
+                UnexportPin(pin);
+            }
+        }
+        #endregion
+
+        #region Class Methods
+        /// <summary>
+        /// Write a value to the pin
+        /// </summary>
+        /// <param name="value">The value to write to the pin</param>
+        public override void Write(bool value)
+        {
+            // Call the static method
+            Write(_pin, value);
+        }
+
+        /// <summary>
+        /// Read a value from the pin
+        /// </summary>
+        /// <returns>The value read from the pin</returns>
+        public override bool Read()
+        {
+            // Call the static method
+            return Read(_pin);
+        }
+
+        /// <summary>
+        /// Dispose of the GPIO pin
+        /// </summary>
+        public override void Dispose()
+        {
+            UnexportPin(_pin);
+        }
+        #endregion
+    }
+}
